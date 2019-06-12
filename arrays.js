36397013164fdef6c5877d46a086b3e11e5a59f2
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-12 20:24:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(x) {
-  return (x, ...food)
+  return [x, ...food]
 }
 
 function destructivelyAddElementToBeginningOfArray(x) {
   food.unshift(x);
   return food
 }
+
+function addElementToEndOfArray(x)
